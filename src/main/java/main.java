5ab9c8e47838aee3ca86a4364172b319a382d9c0
Tatyana563 <nikolaev.english@gmail.com>
2020-02-09 19:18:25 +0200

    Init

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("test gitignore");
+    }
+}
